dd0bfeec819803f67c429fbdb196aa9eba7af15d
Vineet panwar <vineetpanwar027@gmail.com>
2021-11-25 14:35:14 +0530

    Update bubbleSort.js

--- datastructures/sorting/bubbleSort.js
+++ datastructures/sorting/bubbleSort.js
@@ -1,3 +1,25 @@
+function bubbleSort(arr){
+  var noSwaps ;
+  for(var i = arr.length - 1; i >=0; i--) {
+    noSwaps = true;
+    for(var j = 0; j <= i; j++) {
+      if(arr[j] > arr[j+1]) {
+        [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr
+}
+
+
+console.log(bubbleSort([3,1,2,5,6,0]))
+console.log(bubbleSort([-1,7,4,-4,6,5,-8]))
+
+
+//there is also one more approach
+
 //this file has the polyfill code of bubble sorting
 //we traverse form left to right and the bigger number are swapped to the right
 //we do this recursively until the swwpping has taken place
@@ -21,4 +43,3 @@ const bubbleSort = (inpArray) => {
     return returnVal;
 }
 console.log(bubbleSort([3,7,4,4,6,5,8]))
-console.log(bubbleSort([-1,7,4,-4,6,5,-8]))
